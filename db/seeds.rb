11d8ec9df990561e0c983e3c3f890588e9f33554
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-26 11:22:59 -0300

    Added an admin account to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name:"admin", email: "admin@mail.com", password: "administrador", password_confirmation: "administrador", admin: true)
