ac2699ea561a2774d62c97917abf4e93e75f9ca6
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-25 21:00:40 -0300

    Added a route to user stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :stories
+
+  get 'stories/:id', to: 'stories#user_stories', as: 'user_stories'
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
